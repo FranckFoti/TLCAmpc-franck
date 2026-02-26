99de1ffd5ef14b6af1fbe940f2fab1af219a7e5f
Linda Mümken <linda.klesper@gmx.de>
2026-02-27 00:58:11 +0100

    implement ConflictDetector, onflictEvasionCentralCoordinator and ConflictEvasionDistributedCoordinator move conflict_detection to utils

--- src/drone_sim/simulation/__init__.py
+++ src/drone_sim/simulation/__init__.py
@@ -2,3 +2,5 @@
 import drone_sim.simulation.centralized.coordinator  # noqa: F401
 import drone_sim.simulation.distributed.distributed_coordinator  # noqa: F401
 import drone_sim.simulation.distributed.threaded_coordinator  # noqa: F401
+import drone_sim.simulation.centralized.conflict_evasion_coordinator  # noqa: F401
+import drone_sim.simulation.distributed.conflict_evasion_coordinator  # noqa: F401
